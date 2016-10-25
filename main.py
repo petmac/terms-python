23d75b8f19d418fa83c693e168f75b56d721ace9
Peter Mackay <peter.mackay@fanduel.com>
2016-10-25 16:47:23 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import urllib
+import urlparse
+import os
+
+def download(url):
+    response = urllib.urlopen(url)
+    html = response.read()
+    return html
+
+def compute_path(url):
+    u = urlparse.urlparse(url)
+    path = u.path.split("/")
+    parts = filter(bool, ["output"] + [u.hostname] + path)
+    return "/".join(parts)
+
+def select(html, selector):
+    soup = BeautifulSoup(html, "html.parser")
+    selected_tags = soup.select(selector)
+    selected_strings = map(str, selected_tags)
+    parts = "\n".join(selected_strings)
+    return parts
+
+def process(url_and_selector):
+    url = url_and_selector[0]
+    selector = url_and_selector[1]
+    print url
+    path = compute_path(url)
+    html = download(url)
+    selected = select(html, selector)
+    dir = os.path.dirname(path)
+    if dir and not os.path.exists(dir):
+        os.makedirs(dir)
+    with open(path, "w") as f:
+        f.write(selected)
+
+config = {
+    "http://www.example.com/": "body"
+}
+
+if __name__ == '__main__':
+    map(process, config.items())
