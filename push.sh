c75625437aa897b039175b34b9bf435031cfeb89
Peter Mackay <peter.mackay@fanduel.com>
2016-10-26 13:46:01 +0100

    Move SSH setup from push.h to .travis.yml.

--- push.sh
+++ push.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -ve
 
-eval "$(ssh-agent -s)"
-chmod 600 deploy_key.pem
-ssh-add deploy_key.pem
-
 pushd output
 git push git@github.com:petmac/terms-python-output.git
 popd
